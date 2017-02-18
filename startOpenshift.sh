26342a6303fe261c7dd65491540e4343df70c269
Ulrich Schlüter <ulrich.schlueter@gmail.com>
2017-02-18 19:35:58 +0100

    Add more startCommands

--- startOpenshift.sh
+++ startOpenshift.sh
@@ -4,4 +4,4 @@ docker run -d --name "origin" \
         --privileged --pid=host --net=host \
         -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /var/lib/docker:/var/lib/docker:rw \
         -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes:rslave \
-        openshift/origin start
+        openshift/origin:v1.4.1 start
